6c4d54de1bcc83141f1412d4cd1b20e939f001ed
smile-feifan <1424314249@qq.com>
2021-07-03 19:14:11 +0800

    feat:pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,14 @@ module.exports = {
     ['link',{rel:'icon',href:'favicon.png'}],
     ['meta',{name:'author',content:'尽头的另一个我'}],
     ['meta',{name:'keywords',content:'vuepress 介绍,vuepress 说明,尽头的另一个我'}],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   plugins: [
     [
@@ -19,6 +27,16 @@ module.exports = {
           return moment(timestamp).format("LLLL")
         }
       }
+    ],
+    [
+    '@vuepress/pwa', 
+    {      
+     serviceWorker: true,
+     updatePopup: {
+       message: "发现新的功能可用",
+       buttonText: "刷新"
+     }
+   }
     ]
   ],
   themeConfig: {
